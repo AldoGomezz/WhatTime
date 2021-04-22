4846a9c6472f8c0b72ec7719df57b0b49c6fd3ed
AldoGomezz <aldojhairgomez@gmail.com>
2021-04-21 22:29:32 -0500

    Proyecto Creado

---
+++ src/test/java/com/example/whattime/WhatTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whattime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
