ddd43cb0f38539711e620fdc50408c0c097d9d0c
aelvismorales <favtio123@gmail.com>
2021-04-29 13:02:20 -0500

    Agregando Carpeta Repositories, agregando repositorio de usuario

---
+++ src/main/java/com/example/whattime/repositories/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package com.example.whattime.repositories;
+
+import com.example.whattime.entities.Usuario;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+//Solo metodo que hagan un insert select, drop to do contra la base de datos
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Long>
+{
+    Optional<Usuario> findByNombre(String nombre);
+    List<Usuario> findAllByNombre(Iterable<String> iterable);
+}
