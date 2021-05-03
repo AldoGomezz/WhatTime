c0cdd0011177d21828ee23f8b87e1e18661d8bac
AldoGomezz <aldojhairgomez@gmail.com>
2021-05-03 15:03:28 -0500

    CalendarioRepository

---
+++ src/main/java/com/example/whattime/repositories/CalendarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.whattime.repositories;
+
+import com.example.whattime.entities.Calendario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CalendarioRepository extends JpaRepository<Calendario, Long> {
+}
