1486b96bad16095a3176d631f225e2e80fc8bee9
AldoGomezz <aldojhairgomez@gmail.com>
2021-05-03 14:52:38 -0500

    Correcion de entidad calendario

--- src/main/java/com/example/whattime/entities/Calendario.java
+++ src/main/java/com/example/whattime/entities/Calendario.java
@@ -1,41 +1,39 @@
 package com.example.whattime.entities;
 
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
-@Table(name = "calendario",
-        uniqueConstraints = {
-                @UniqueConstraint(name = "calendario_name_unique", columnNames = "name_calendario")
-        })
+@Table(name = "calendarios",uniqueConstraints = {@UniqueConstraint(name = "calendario_name_unique",
+        columnNames = "name_calendario")})
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class Calendario {
     @Id
-    @SequenceGenerator(name = "calendario_sequence",
-            sequenceName = "calendario_sequence",
+    @SequenceGenerator(name = "calendario_sequence", sequenceName = "calendario_sequence",
             allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "calendario_sequence")
 
-    @Column(name="id", updatable = false)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "calendario_sequence")
 
-    @Column(name="name_calendario",nullable = false,columnDefinition = "TEXT")
+    @Column(name = "id", updatable = false)
+    private long id;
+
+    @Column(name = "name_calendario", nullable = false, columnDefinition = "TEXT")
     private String name_calendario;
 
-    @OneToOne(cascade=CascadeType.ALL)
+    @OneToOne
     @JoinColumn(
             name = "usuario_id",
             nullable = false,
             referencedColumnName = "id",
             foreignKey = @ForeignKey(
-                    name = "usuario_calendar_fk"
+                    name = "usuario_calendario_fk"
             )
     )
     private Usuario usuario;
