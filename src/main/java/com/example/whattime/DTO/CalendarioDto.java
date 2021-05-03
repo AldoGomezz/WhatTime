f3407a25d9c5b2f8002d6bc5c1ac80ef1f534b63
AldoGomezz <aldojhairgomez@gmail.com>
2021-05-03 15:00:20 -0500

    CalendarioDto

---
+++ src/main/java/com/example/whattime/DTO/CalendarioDto.java
@@ -0,0 +1,12 @@
+package com.example.whattime.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class CalendarioDto {
+    private Long id;
+    private String name_calendario;
+}
