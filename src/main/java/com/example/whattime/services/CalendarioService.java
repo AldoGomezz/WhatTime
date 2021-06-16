e6986b8996fc0dc9c32dbfdfa034fda925ca5585
AldoGomezz <aldojhairgomez@gmail.com>
2021-06-15 23:15:09 -0500

    Recibir las notas del usuario

--- src/main/java/com/example/whattime/services/CalendarioService.java
+++ src/main/java/com/example/whattime/services/CalendarioService.java
@@ -2,10 +2,14 @@ package com.example.whattime.services;
 
 import com.example.whattime.DTO.CalendarioDto;
 import com.example.whattime.DTO.CreateCalendarioDto;
+import com.example.whattime.DTO.NotaDto;
+import com.example.whattime.entities.Usuario;
 import com.example.whattime.exceptions.WhatTimeExceptions;
+
+import java.util.List;
 
 public interface CalendarioService {
     CalendarioDto createCalendario(CreateCalendarioDto createCalendarioDto, String nombre) throws WhatTimeExceptions;
     CalendarioDto updateCalendario(CalendarioDto calendarioDto) throws WhatTimeExceptions;
-    
+    List<NotaDto> getNotesFromUser(Long UserID) throws WhatTimeExceptions;
 }
